83726445e2711fb6b8ef5524100974c572278ef2
chesterkuo <chester.kuo@gmail.com>
2019-06-16 14:15:54 +0000

    update

--- koa-sqlite-jwt-server/index.js
+++ koa-sqlite-jwt-server/index.js
@@ -6,7 +6,7 @@ const cors = require('@koa/cors');
 const errorHandler = require('./middleware/errorHandler');
 const authenticated = require('./middleware/authenticated');
 const authRoute = require('./routes/auth');
-//const petsRoute = require('./routes/pets');
+const petsRoute = require('./routes/pets');
 const testRoute = require('./routes/test');
 
 const app = new Koa();
@@ -16,6 +16,7 @@ app.use(errorHandler);
 app.use(cors());
 
 router.post('/auth', bodyParser(), authRoute);
+router.get('/my-pets', authenticated, petsRoute);
 router.get('/test', authenticated, testRoute);
 
 app
