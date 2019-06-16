14e4fd39c16bbb7d2e1117df0a2d9b53a32b1e46
chesterkuo <chester.kuo@gmail.com>
2019-06-16 12:53:57 +0000

    update koq-jwt case

---
+++ koa-sqlite-jwt-server/index.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+
+const errorHandler = require('./middleware/errorHandler');
+const authenticated = require('./middleware/authenticated');
+const authRoute = require('./routes/auth');
+const petsRoute = require('./routes/pets');
+
+const app = new Koa();
+const router = new Router();
+
+app.use(errorHandler);
+app.use(cors());
+
+router.post('/auth', bodyParser(), authRoute);
+router.get('/my-pets', authenticated, petsRoute);
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(process.env.PORT || 7000);
